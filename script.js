ef49172480336c882c8146d2dd0e50269192e50d
Cralinhos <carlosaugustocostadesousa6@gmail.com>
2024-10-02 15:30:35 -0300

    atualização 2 OUT 2024

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
-// Obtém elementos principais
 const constructedPhrase = document.getElementById('constructed-phrase');
 const clearButton = document.getElementById('clear-btn');
 const letters = document.querySelectorAll('.letter');
 
-// Função para adicionar letras à frase
 letters.forEach(letter => {
     letter.addEventListener('click', () => {
         const selectedLetter = letter.getAttribute('data-letter');
@@ -11,25 +9,25 @@ letters.forEach(letter => {
     });
 });
 
-// Função para limpar a frase
 clearButton.addEventListener('click', () => {
     constructedPhrase.innerHTML = '';
 });
 
-// script.js
 document.addEventListener('DOMContentLoaded', () => {
     const clearLetterButton = document.getElementById('clear-letra');
     const constructedPhraseDiv = document.getElementById('constructed-phrase');
-
     clearLetterButton.addEventListener('click', () => {
-        // Obtém o texto atual da frase construída
         let currentPhrase = constructedPhraseDiv.textContent;
-
-        // Remove a última letra da frase
         if (currentPhrase.length > 0) {
-            currentPhrase = currentPhrase.slice(0, -1); // Remove a última letra
-            constructedPhraseDiv.textContent = currentPhrase; // Atualiza a frase
+            currentPhrase = currentPhrase.slice(0, -1);
+            constructedPhraseDiv.textContent = currentPhrase;
         }
     });
 });
+
+document.getElementById("espaco-letra").addEventListener("click", function() {
+    let fraseAtual = constructedPhrase.textContent;
+    constructedPhrase.innerHTML = fraseAtual + " ";
+});
+
 
